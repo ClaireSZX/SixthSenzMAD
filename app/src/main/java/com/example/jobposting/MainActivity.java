568fc20e85fb38040a02e7e68791a9aff89e5649
ClaireSZX <24066453@siswa.um.edu.my>
2026-01-06 00:15:29 +0800

    Job Posting fix

--- app/src/main/java/com/example/jobposting/MainActivity.java
+++ app/src/main/java/com/example/jobposting/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.jobposting.R;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
